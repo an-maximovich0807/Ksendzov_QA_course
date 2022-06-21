17a3ea11e6619b5a6db5665ed5ff5bf20a6f186b
an-maximovich0807 <an.maximovich0807@gmail.com>
2022-06-21 20:35:33 +0300

    Correct the mistakes

--- JS_HW1/JS_HW1_by_Stasie0807.js
+++ JS_HW1/JS_HW1_by_Stasie0807.js
@@ -10,7 +10,7 @@ console.log(item_3);
 let item_4 = "Yolochka";
 console.log(item_4);
 
-console.log(item_3 + " " + item_4);
+console.log(item_3 + item_4);
 console.log(item_3 * item_4); // мы перемножаем счисла на строки?
 
 let item_5 = item_3;
@@ -22,7 +22,7 @@ let item_7 = String(item_6); // не уверена в правильности 
 let item_7_type = typeof item_7;
 console.log("item_7 == ", item_7, "item_7_type == ", item_7_type);
 
-let age_1 = 10; // выбирала значения для проверки 19, 61
+let age_1 = 60; // выбирала значения для проверки 19, 61
 let age_2 = 18;
 let age_3 = 60;
 
@@ -34,7 +34,7 @@ if (age_1 < age_2) {
             " It's less then " +
             age_2
     );
-} else if (age_2 >= age_1 < age_3) {
+} else if ((age_2 >= age_1, age_1 < age_3)) {
     console.log("Welcome!");
 } else if (age_1 > age_3) {
     console.log("Keep calm and watch Culture channel");
