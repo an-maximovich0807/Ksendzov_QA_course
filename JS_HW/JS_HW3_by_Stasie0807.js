a8b9b980dfffd4f76d84b14be2962ef189d52132
an-maximovich0807 <an.maximovich0807@gmail.com>
2022-07-25 12:29:22 +0300

    edit JS_HW3 file

--- JS_HW/JS_HW3_by_Stasie0807.js
+++ JS_HW/JS_HW3_by_Stasie0807.js
@@ -93,357 +93,102 @@ console.log(
     "--------------------------------Task#2--------------------------------"
 );
 
-const base = [
+const fs = require("fs");
+
+let file_cont = fs.readFileSync("task2.json", "utf-8");
+let data = JSON.parse(file_cont);
+
+function removeDuplicates(data, key) {
+    return [...new Map(data.map((item) => [key(item), item])).values()];
+}
+
+console.log(removeDuplicates(data, (item) => item.name));
+
+console.log(
+    "--------------------------------Task#3.1--------------------------------"
+);
+
+const enterprises = [
     {
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz",
-        address: {
-            street: "Kulas Light",
-            suite: "Apt. 556",
-            city: "Gwenborough",
-            zipcode: "92998-3874",
-            geo: {
-                lat: "-37.3159",
-                lng: "81.1496",
+        id: 1,
+        name: "Предприятие 1",
+        departments: [
+            {
+                id: 2,
+                name: "Отдел тестирования",
+                employees_count: 10,
             },
-        },
-        phone: "1-770-736-8031 x56442",
-        website: "hildegard.org",
-        company: {
-            name: "Romaguera-Crona",
-            catchPhrase: "Multi-layered client-server neural-net",
-            bs: "harness real-time e-markets",
-        },
+            {
+                id: 3,
+                name: "Отдел маркетинга",
+                employees_count: 20,
+            },
+            {
+                id: 4,
+                name: "Администрация",
+                employees_count: 15,
+            },
+        ],
     },
     {
-        name: "Clementine Bauch",
-        username: "Samantha",
-        email: "Nathan@yesenia.net",
-        address: {
-            street: "Douglas Extension",
-            suite: "Suite 847",
-            city: "McKenziehaven",
-            zipcode: "59590-4157",
-            geo: {
-                lat: "-68.6102",
-                lng: "-47.0653",
+        id: 5,
+        name: "Предприятие 2",
+        departments: [
+            {
+                id: 6,
+                name: "Отдел разработки",
+                employees_count: 50,
             },
-        },
-        phone: "1-463-123-4447",
-        website: "ramiro.info",
-        company: {
-            name: "Romaguera-Jacobson",
-            catchPhrase: "Face to face bifurcated interface",
-            bs: "e-enable strategic applications",
-        },
+            {
+                id: 7,
+                name: "Отдел маркетинга",
+                employees_count: 20,
+            },
+            {
+                id: 8,
+                name: "Отдел охраны труда",
+                employees_count: 5,
+            },
+        ],
     },
     {
-        name: "Ervin Howell",
-        username: "Antonette",
-        email: "Shanna@melissa.tv",
-        address: {
-            street: "Victor Plains",
-            suite: "Suite 879",
-            city: "Wisokyburgh",
-            zipcode: "90566-7771",
-            geo: {
-                lat: "-43.9509",
-                lng: "-34.4618",
+        id: 9,
+        name: "Предприятие 3",
+        departments: [
+            {
+                id: 10,
+                name: "Отдел аналитики",
+                employees_count: 0,
             },
-        },
-        phone: "010-692-6593 x09125",
-        website: "anastasia.net",
-        company: {
-            name: "Deckow-Crist",
-            catchPhrase: "Proactive didactic contingency",
-            bs: "synergize scalable supply-chains",
-        },
-    },
-    {
-        name: "Clementine Bauch",
-        username: "Samantha",
-        email: "Nathan@yesenia.net",
-        address: {
-            street: "Douglas Extension",
-            suite: "Suite 847",
-            city: "McKenziehaven",
-            zipcode: "59590-4157",
-            geo: {
-                lat: "-68.6102",
-                lng: "-47.0653",
-            },
-        },
-        phone: "1-463-123-4447",
-        website: "ramiro.info",
-        company: {
-            name: "Romaguera-Jacobson",
-            catchPhrase: "Face to face bifurcated interface",
-            bs: "e-enable strategic applications",
-        },
-    },
-    {
-        name: "Patricia Lebsack",
-        username: "Karianne",
-        email: "Julianne.OConner@kory.org",
-        address: {
-            street: "Hoeger Mall",
-            suite: "Apt. 692",
-            city: "South Elvis",
-            zipcode: "53919-4257",
-            geo: {
-                lat: "29.4572",
-                lng: "-164.2990",
-            },
-        },
-        phone: "493-170-9623 x156",
-        website: "kale.biz",
-        company: {
-            name: "Robel-Corkery",
-            catchPhrase: "Multi-tiered zero tolerance productivity",
-            bs: "transition cutting-edge web services",
-        },
-    },
-    {
-        name: "Chelsey Dietrich",
-        username: "Kamren",
-        email: "Lucio_Hettinger@annie.ca",
-        address: {
-            street: "Skiles Walks",
-            suite: "Suite 351",
-            city: "Roscoeview",
-            zipcode: "33263",
-            geo: {
-                lat: "-31.8129",
-                lng: "62.5342",
-            },
-        },
-        phone: "(254)954-1289",
-        website: "demarco.info",
-        company: {
-            name: "Keebler LLC",
-            catchPhrase: "User-centric fault-tolerant solution",
-            bs: "revolutionize end-to-end systems",
-        },
-    },
-    {
-        name: "Clementine Bauch",
-        username: "Samantha",
-        email: "Nathan@yesenia.net",
-        address: {
-            street: "Douglas Extension",
-            suite: "Suite 847",
-            city: "McKenziehaven",
-            zipcode: "59590-4157",
-            geo: {
-                lat: "-68.6102",
-                lng: "-47.0653",
-            },
-        },
-        phone: "1-463-123-4447",
-        website: "ramiro.info",
-        company: {
-            name: "Romaguera-Jacobson",
-            catchPhrase: "Face to face bifurcated interface",
-            bs: "e-enable strategic applications",
-        },
-    },
-    {
-        name: "Mrs. Dennis Schulist",
-        username: "Leopoldo_Corkery",
-        email: "Karley_Dach@jasper.info",
-        address: {
-            street: "Norberto Crossing",
-            suite: "Apt. 950",
-            city: "South Christy",
-            zipcode: "23505-1337",
-            geo: {
-                lat: "-71.4197",
-                lng: "71.7478",
-            },
-        },
-        phone: "1-477-935-8478 x6430",
-        website: "ola.org",
-        company: {
-            name: "Considine-Lockman",
-            catchPhrase: "Synchronised bottom-line interface",
-            bs: "e-enable innovative applications",
-        },
-    },
-    {
-        name: "Kurtis Weissnat",
-        username: "Elwyn.Skiles",
-        email: "Telly.Hoeger@billy.biz",
-        address: {
-            street: "Rex Trail",
-            suite: "Suite 280",
-            city: "Howemouth",
-            zipcode: "58804-1099",
-            geo: {
-                lat: "24.8918",
-                lng: "21.8984",
-            },
-        },
-        phone: "210.067.6132",
-        website: "elvis.io",
-        company: {
-            name: "Johns Group",
-            catchPhrase: "Configurable multimedia task-force",
-            bs: "generate enterprise e-tailers",
-        },
-    },
-    {
-        name: "Clementina DuBuque",
-        username: "Moriah.Stanton",
-        email: "Rey.Padberg@karina.biz",
-        address: {
-            street: "Kattie Turnpike",
-            suite: "Suite 198",
-            city: "Lebsackbury",
-            zipcode: "31428-2261",
-            geo: {
-                lat: "-38.2386",
-                lng: "57.2232",
-            },
-        },
-        phone: "024-648-3804",
-        website: "ambrose.net",
-        company: {
-            name: "Hoeger LLC",
-            catchPhrase: "Centralized empowering task-force",
-            bs: "target end-to-end models",
-        },
-    },
-    {
-        name: "Leanne Graham",
-        username: "Bret",
-        email: "Sincere@april.biz",
-        address: {
-            street: "Kulas Light",
-            suite: "Apt. 556",
-            city: "Gwenborough",
-            zipcode: "92998-3874",
-            geo: {
-                lat: "-37.3159",
-                lng: "81.1496",
-            },
-        },
-        phone: "1-770-736-8031 x56442",
-        website: "hildegard.org",
-        company: {
-            name: "Romaguera-Crona",
-            catchPhrase: "Multi-layered client-server neural-net",
-            bs: "harness real-time e-markets",
-        },
-    },
-    {
-        name: "Nicholas Runolfsdottir V",
-        username: "Maxime_Nienow",
-        email: "Sherwood@rosamond.me",
-        address: {
-            street: "Ellsworth Summit",
-            suite: "Suite 729",
-            city: "Aliyaview",
-            zipcode: "45169",
-            geo: {
-                lat: "-14.3990",
-                lng: "-120.7677",
-            },
-        },
-        phone: "586.493.6943 x140",
-        website: "jacynthe.com",
-        company: {
-            name: "Abernathy Group",
-            catchPhrase: "Implemented secondary concept",
-            bs: "e-enable extensible e-tailers",
-        },
-    },
-    {
-        name: "Clementine Bauch",
-        username: "Samantha",
-        email: "Nathan@yesenia.net",
-        address: {
-            street: "Douglas Extension",
-            suite: "Suite 847",
-            city: "McKenziehaven",
-            zipcode: "59590-4157",
-            geo: {
-                lat: "-68.6102",
-                lng: "-47.0653",
-            },
-        },
-        phone: "1-463-123-4447",
-        website: "ramiro.info",
-        company: {
-            name: "Romaguera-Jacobson",
-            catchPhrase: "Face to face bifurcated interface",
-            bs: "e-enable strategic applications",
-        },
-    },
-    {
-        name: "Glenna Reichert",
-        username: "Delphine",
-        email: "Chaim_McDermott@dana.io",
-        address: {
-            street: "Dayna Park",
-            suite: "Suite 449",
-            city: "Bartholomebury",
-            zipcode: "76495-3109",
-            geo: {
-                lat: "24.6463",
-                lng: "-168.8889",
-            },
-        },
-        phone: "(775)976-6794 x41206",
-        website: "conrad.com",
-        company: {
-            name: "Yost and Sons",
-            catchPhrase: "Switchable contextually-based project",
-            bs: "aggregate real-time technologies",
-        },
-    },
-    {
-        name: "Clementina DuBuque",
-        username: "Moriah.Stanton",
-        email: "Rey.Padberg@karina.biz",
-        address: {
-            street: "Kattie Turnpike",
-            suite: "Suite 198",
-            city: "Lebsackbury",
-            zipcode: "31428-2261",
-            geo: {
-                lat: "-38.2386",
-                lng: "57.2232",
-            },
-        },
-        phone: "024-648-3804",
-        website: "ambrose.net",
-        company: {
-            name: "Hoeger LLC",
-            catchPhrase: "Centralized empowering task-force",
-            bs: "target end-to-end models",
-        },
-    },
-    {
-        name: "Mrs. Dennis Schulist",
-        username: "Leopoldo_Corkery",
-        email: "Karley_Dach@jasper.info",
-        address: {
-            street: "Norberto Crossing",
-            suite: "Apt. 950",
-            city: "South Christy",
-            zipcode: "23505-1337",
-            geo: {
-                lat: "-71.4197",
-                lng: "71.7478",
-            },
-        },
-        phone: "1-477-935-8478 x6430",
-        website: "ola.org",
-        company: {
-            name: "Considine-Lockman",
-            catchPhrase: "Synchronised bottom-line interface",
-            bs: "e-enable innovative applications",
-        },
+        ],
     },
 ];
+
+const empl_counter = function (number) {
+    let lastNumber = number.toString().split("").pop();
+    if (lastNumber && number) {
+        if (lastNumber == 1) return `${number} сотрудник`;
+        else if (lastNumber > 1 && lastNumber <= 4)
+            return `${number} сотрудникa`;
+        else return `${number} сотрудников`;
+    } else return "сотрудников нет";
+};
+
+const getDepts = function (companies) {
+    companies.forEach((company) => {
+        let depts = [];
+        let count = 0;
+        depts.push(company.name);
+        if (company.departments) {
+            company.departments.forEach((dept) => {
+                count += +dept.employees_count;
+                depts.push(
+                    `-${dept.name}(${empl_counter(dept.employees_count)})`
+                );
+            });
+            depts[0] += `(${empl_counter(count)})`;
+        }
+        console.log(depts.join(`\n `));
+    });
+};
+getDepts(enterprises);
